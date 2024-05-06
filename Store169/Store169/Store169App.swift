f449929c02235839b055f7fe05aef5d2ed1826ee
Anastasiya Omak <ios.developer.by@gmail.com>
2024-05-06 13:14:15 +0300

    Init commit

---
+++ Store169/Store169/Store169App.swift
@@ -0,0 +1,17 @@
+//
+//  Store169App.swift
+//  Store169
+//
+//  Created by Anastasiya Omak on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Store169App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
