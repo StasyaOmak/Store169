e37692c21d47efe67ec9d9d4d75220f9ca54fa21
Anastasiya Omak <ios.developer.by@gmail.com>
2024-05-08 11:57:35 +0300

    Закончена работа над Task1

---
+++ Store169/Store169/DetailModule/Model/Furniture.swift
@@ -0,0 +1,22 @@
+//
+//  Furniture.swift
+//  Store169
+//
+//  Created by Anastasiya Omak on 07/05/2024.
+//
+
+import Foundation
+
+/// Еденица мебели
+struct Furniture {
+    /// Название мебели
+    let name: String
+    /// Название изображения мебели
+    let imageName: String
+    /// Цена мебели
+    let price: Int
+    /// Артикул мебели
+    let article: Int
+    /// Описание мебели
+    let description: String
+}
